635cdad85243f225f0016c41100b1842a9f57e51
Mahmoud <mhd-10@hotmail.com>
2024-01-24 11:15:43 +0300

    up

--- main.py
+++ main.py
@@ -198,7 +198,6 @@ def step_3_method():
 def step_4():
     """
     fill rest of the gaps
-    get
     """
 
 
@@ -206,11 +205,12 @@ if __name__ == "__main__":
     """
     Loop through all the ids, find all close sales, with the same industry and location type with x distance
     """
-    step_1()
-    step_2()
+    # step_1()
+    # step_2()
 
     """ step_3() """
-    step_3_method()
+    # step_3_method()
+    step_4()
 
     # reference_ids = list(
     #     new_sales_collection.distinct(
